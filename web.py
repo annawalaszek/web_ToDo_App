7eaaf5a21a70e978301625108547208abecd9657
Anna Walaszek <a.e.walaszek@gmail.com>
2023-03-18 22:38:36 +0100

    Add new page

--- web.py
+++ web.py
@@ -10,15 +10,16 @@ def add_todo():
     todos_list.append(_)
     Foo.set_todos(list=todos_list)
 
+streamlit.set_page_config(layout='wide')
 
 streamlit.title('Todo App')
 streamlit.subheader('Simple web app to write down a task list')
+
 streamlit.write(
-    'Made to increase <b>productivity and decrease</b> stress :)')
-streamlit.text_input(label='Add task',
-                     placeholder='Write here new todo...',
-                     on_change=add_todo,
-                     key='new_task')
+    'Made to increase <b><strong>productivity</strong></b> '
+    'and decrease stress :)',
+    unsafe_allow_html=True)
+
 for number, todo in enumerate(todos_list):
     checkbox = streamlit.checkbox(todo, key=todo)
     if checkbox:
@@ -26,5 +27,10 @@ for number, todo in enumerate(todos_list):
         Foo.set_todos(list=todos_list)
         del streamlit.session_state[todo]
         streamlit.experimental_rerun()
+
+streamlit.text_input(label='Add task',
+                     placeholder='Write here new todo...',
+                     on_change=add_todo,
+                     key='new_task')
 
 # streamlit.session_state
